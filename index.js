7dbea068e4e2faba92554abcecc27d3c1a49a64f
William Yeung <williamyeung1141998@gmail.com>
2020-01-26 21:53:43 -0500

    created alternative solution using async/await

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ function openAndCloseModal(cardDiv, pictureSrc, fullName, email, city, state, ph
         document.querySelector(".modal-text:nth-of-type(4)").textContent = fullAddress;
         document.querySelector(".modal-text:nth-of-type(5)").textContent = birth;
         modalContainer.style.display = "block";
+        
     })
     modalContainer.querySelector(".modal-close-btn").addEventListener("click", function() {
         modalContainer.style.display = "none"
@@ -57,10 +58,6 @@ function createModal() {
                                         <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
                                         <p class="modal-text">Birthday: 10/21/2015</p>
                                     </div>
-                                </div>
-                                <div class="modal-btn-container">
-                                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
                                 </div>`
     document.body.insertBefore(modalContainer, document.querySelector("[src='index.js']"));
 }
@@ -77,84 +74,96 @@ function formatDate(dateString) {
 
 
 
-/*Fetched Data*/
-fetch("https://randomuser.me/api?format=json&results=12&nat=us") // returns promise with resolve value of a http response object
-// since fetch doesnt catch http status errors, we have to check for that
-.then(response => {
-    if (!response.ok) {
-        return Promise.reject(new Error(response.statusText))
-    } else {
-        return Promise.resolve(response)
-    }
-})
-.then(response => response.json())
-.then(data => {
-    let cardDiv;
-    let cardArray = [];
-    createSearch();
-    createModal();
+// /*Fetched Data*/
+// fetch("https://randomuser.me/api?format=json&results=12&nat=us") // returns promise with resolve value of a http response object
+// // since fetch doesnt catch http status errors, we have to check for that
+// .then(response => {
+//     if (!response.ok) {
+//         return Promise.reject(new Error(response.statusText))
+//     } else {
+//         return Promise.resolve(response)
+//     }
+// })
+// .then(response => response.json())
+// .then(data => {
+//     let cardDiv;
+//     let cardArray = [];
+//     createSearch();
+//     createModal();
 
-    data.results.forEach(person => {
-        let picture = person.picture.thumbnail;
-        let name = `${person.name.first} ${person.name.last}`;
-        let email = person.email;
-        let city = `${person.location.city}`
-        let state = `${person.location.state}`;
-        let fullAddress = `${person.location.street.number} ${person.location.street.name}, ${city} ${state} ${person.location.postcode}`
-        let phone = person.phone;
-        let birth = formatDate(person.dob.date);
+//     data.results.forEach(person => {
+//         let picture = person.picture.thumbnail;
+//         let name = `${person.name.first} ${person.name.last}`;
+//         let email = person.email;
+//         let city = `${person.location.city}`
+//         let state = `${person.location.state}`;
+//         let fullAddress = `${person.location.street.number} ${person.location.street.name}, ${city} ${state} ${person.location.postcode}`
+//         let phone = person.phone;
+//         let birth = formatDate(person.dob.date);
         
-        let card = createCard(cardDiv, picture, name, email, city, state, phone, fullAddress, birth);
-        cardArray.push(card);
-        document.querySelector("#gallery").appendChild(card)
-    });
-    return cardArray
-})
-.then(results => {
-    let prev = document.querySelector("#modal-prev");
-    let next = document.querySelector("#modal-next");
-
-    
-    let modal = document.querySelector(".modal");
-    let currentCard = results.find(card => card.innerHTML.includes(modal.querySelector("#name").textContent))
-    let currentIndex = results.indexOf(currentCard);
-    console.log(currentIndex)
-    
-    // prev.addEventListener("click", function() {
-    //     if (currentIndex !== 0) {
-    //         //document.querySelector(".modal-img").src = results[currentIndex - 1].querySelector("img").src;
-    //         // document.querySelector(".modal-name").textContent = fullName;
-    //         // document.querySelector(".modal-text:nth-of-type(1)").textContent = email;
-    //         // document.querySelector(".modal-text:nth-of-type(2)").textContent = city; 
-    //         // document.querySelector(".modal-text:nth-of-type(3)").textContent = phone;
-    //         // document.querySelector(".modal-text:nth-of-type(4)").textContent = fullAddress;
-    //         // document.querySelector(".modal-text:nth-of-type(5)").textContent = birth;
-    //     } else {
-    //         this.style.backgroundColor = 'red'
-    //     }
-    // })
-
-
-    
-
-    document.querySelector("#search-input").addEventListener("input", function() {
-        let filteredData = results.forEach(person => {
-            let name = person.querySelector("#name");
-           if (!name.innerHTML.toLowerCase().includes(this.value.toLowerCase())) {
-                person.style.display = "none"
-           } else {
-               person.style.display = "flex"
-           }
-        })
-    });
-})
-.catch(error => console.log("nooooo", error));
+//         let card = createCard(cardDiv, picture, name, email, city, state, phone, fullAddress, birth);
+//         cardArray.push(card);
+//         document.querySelector("#gallery").appendChild(card)
+//     });
+//     return cardArray
+// })
+// .then(results => {
+//     document.querySelector("#search-input").addEventListener("input", function() {
+//         let filteredData = results.forEach(person => {
+//             let name = person.querySelector("#name");
+//            if (!name.innerHTML.toLowerCase().includes(this.value.toLowerCase())) {
+//                 person.style.display = "none"
+//            } else {
+//                person.style.display = "flex"
+//            }
+//         })
+//     });
+// })
+// .catch(error => console.log("nooooo", error));
 
 
      
+async function getData() {
+    try {
+        let response = await fetch("https://randomuser.me/api?format=json&results=12&nat=us");
+        let data = await response.json();
 
+        let cardDiv;
+        let cardArray = [];
+        createSearch();
+        createModal();
 
+        data.results.forEach(person => {
+            let picture = person.picture.thumbnail;
+            let name = `${person.name.first} ${person.name.last}`;
+            let email = person.email;
+            let city = `${person.location.city}`
+            let state = `${person.location.state}`;
+            let fullAddress = `${person.location.street.number} ${person.location.street.name}, ${city} ${state} ${person.location.postcode}`
+            let phone = person.phone;
+            let birth = formatDate(person.dob.date);
+            
+            let card = createCard(cardDiv, picture, name, email, city, state, phone, fullAddress, birth);
+            cardArray.push(card);
+            document.querySelector("#gallery").appendChild(card)
+        });
+        
+        document.querySelector("#search-input").addEventListener("input", function() {
+            let filteredData = cardArray.forEach(person => {
+                let name = person.querySelector("#name");
+               if (!name.innerHTML.toLowerCase().includes(this.value.toLowerCase())) {
+                    person.style.display = "none"
+               } else {
+                   person.style.display = "flex"
+               }
+            })
+        });
+    } catch (error) {
+        console.log("nooooo", error)
+    }
+}
 
+getData();
 
 
 
